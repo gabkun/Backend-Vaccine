4a892cc1555445e528a89f476a2980fffa1cfccb
gabkun <95224438+gabkun@users.noreply.github.com>
2026-03-16 21:50:16 +0800

    fixed

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ["http://localhost:8000", "http://localhost"], // list of allowed origins
+  origin: ["https://canlandog-vaccination.infinityfreeapp.com", "http://localhost:8000"], // list of allowed origins
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
